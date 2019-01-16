c820d1e88b53f8f2f9ba28572474c7346f50ba7b
Daniel Molnar <molnard0412@gmail.com>
2019-01-16 18:18:11 +0100

    biggernum

---
+++ week-01/day-3/print-bigger/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Write a program that asks for two numbers and prints the bigger one
+    int a, b;
+    std::cout << "Give me two numbers" << std::endl;
+    std::cin >> a >> b;
+    if(a > b){
+        std::cout << "The bigger number is: " << a << std::endl;
+    } else {
+        std::cout << "The bigger number is: " << b << std::endl;
+    }
+    return 0;
+}
