de8d420afd989a850ad0c499c98492e154bc2505
Daniel Molnar <molnard0412@gmail.com>
2019-01-17 18:57:51 +0100

    renamed folder to color + sum&reverse project

---
+++ week-01/day-4/reverse-list/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `aj`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Reverse the order of the elements in `aj`
+    // - Print the elements of the reversed `aj`
+    int aj[] = {3, 4, 5, 6, 7};
+    int k = sizeof(aj) / sizeof(aj[0]);
+    int temp;
+    for(int i = 0; i < k / 2; i++){ // /2 needed -> only k: i=3 its gonna re-reverse the procedure
+        temp = aj[i]; // if i=0 temp will be 3      ||
+        aj[i] = aj[k-i-1]; // then the first element will be equal with the value of 5-0-1 = 4th element (5th) ||
+        aj[k-i-1] = temp; // then the 4th element (5th) will get the value of temp -> 3 ||
+    }
+    for(int i = 0; i < k; i++){
+        std::cout << aj[i] << " ";
+    }
+
+    return 0;
+}
---
+++ week-01/day-4/sum-all/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+    int ai[] = {3, 4, 5, 6, 7};
+    int b = sizeof(ai) / sizeof(ai[0]);
+    int sum = 0;
+    for(int i = 0; i < b; i++){
+        sum += ai[i];
+    }
+    std::cout << "The sum of the elements: " << sum << std::endl;
+    return 0;
+}
