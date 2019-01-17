5d2e1c2e01612f44f2cd3794367df5d0dcf1a651
Daniel Molnar <molnard0412@gmail.com>
2019-01-17 13:38:23 +0100

    sum

---
+++ week-01/day-4/sum/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int sum(int a){
+    int b = 0;
+    for(int i = 0; i <= a; i++){
+        b += i;
+    }
+    return b;
+}
+int main(int argc, char* args[]) {
+
+    // - Write a function called `sum` that sum all the numbers
+    //   until the given parameter and returns with an integer
+    int y;
+    std::cout << "Give me a number" << std::endl;
+    std::cin >> y;
+
+    sum(y);
+
+    std::cout << "The sum of these numbers is " << sum(y) << std::endl;
+
+
+    return 0;
+}
