0df7a11bdc750e69351652ca6bbf7a55a1f9debf
Daniel Molnar <molnard0412@gmail.com>
2019-01-17 17:24:58 +0100

    append-a and gitignore update

---
+++ week-01/day-4/append-a/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `animals`
+    //   with the following content: `["koal", "pand", "zebr"]`
+    // - Add all elements an `"a"` at the end
+    std::string animals[3] = {"koal", "pand", "zebr"};
+    std::string a = "a";
+    for(int i = 0; i < 3; i++){
+        animals[i] += a;
+    }
+    /*
+    for(int i = 0; i < 3; i++){
+        std::cout << animals[i] << std::endl;
+    }
+     */
+
+    return 0;
+}
--- week-01/day-4/functions-and-arrays/main.cpp
+++ week-01/day-4/functions-and-arrays/main.cpp
@@ -12,5 +12,15 @@ int main(int argc, char* args[]) {
     // - In `colors[2]` store the shades of pink:
     //   `"orchid", "violet", "pink", "hot pink"`
 
+    std::string colors[3][5] = {
+            {"line", "forest green", "olive", "pale green", "spring green"},
+            {"orange red", "red", "tomato"},
+            {"orchid", "violit", "pink", "hot pink"}
+    };
+    //example cout
+    std::cout << colors[1][0] << std::endl;
+
+
+
     return 0;
 }
