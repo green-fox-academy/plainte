0b69eaae02827f4faf260f22509500c31bb88794
Daniel Molnar <molnard0412@gmail.com>
2019-01-18 19:52:08 +0100

    diamond fixed

--- week-01/day-3/draw-diamond/main.cpp
+++ week-01/day-3/draw-diamond/main.cpp
@@ -29,10 +29,10 @@ int main(int argc, char* args[]) {
         std::cout << std::endl;
     }
     if (n % 2 == 0) {
-        n -= 1;
+       n -= 1;
     }
     for (int i = (n - 1) / 2 - 1; i >= 0; i--) {
-        for (int j = 0; j <= n / 2 - i - 1; j++) {
+        for (int j = 0; j < n / 2 - i; j++) {
             std::cout << " ";
         }
         for (int j = 0 ; j < 2 * i + 1; j++) {
