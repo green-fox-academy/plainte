2bc56c58845a8ff17616bcd0f66b1601aeb7b799
Daniel Molnar <molnard0412@gmail.com>
2019-01-18 17:19:44 +0100

    diamond in progress

--- week-01/day-3/draw-diamond/main.cpp
+++ week-01/day-3/draw-diamond/main.cpp
@@ -15,31 +15,24 @@ int main(int argc, char* args[]) {
     //    *
     //
     // The diamond should have as many lines as the number was
-    int n, y, i, space;
-    std::cout << "Enter number of rows!" << std::endl;
+    int n;
+    std::cout << "How many lines you want?" << std::endl;
     std::cin >> n;
-    space = n - 1;
+    for (int i = 0; i < n / 2 + 1; i++) {
+        for (int j = i; j < n; j++) {
+            std::cout << " ";
+        }
+        for (int j = 0; j <= (2 * i); j++) {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+    for (int i = n / 2 + 1; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << "%";
+        }
+        std::cout << "\n";
+    }
 
-    for (i = 1; i <= n; i++) {
-        for (y = 1; y <= space; y++) {
-            std::cout << " ";
-        }
-        space--;
-        for (y = 1; y <= 2 * i - 1; y++) {
-            std::cout << "*";
-        }
-        std::cout << "\n";
-    }
-    space = 1;
-    for (i = 1; i <= n; i++) {
-        for (y = 1; y <= space; y++) {
-            std::cout << " ";
-        }
-        space++;
-        for (y = 1; y <= 2 * (n - i) - 1; y++) {
-            std::cout << "*";
-        }
-        std::cout << "\n";
-    }
     return 0;
 }
