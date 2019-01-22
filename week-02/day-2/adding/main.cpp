a96460fee8f722de4bd97d6735648144018213e1
Daniel Molnar <molnard0412@gmail.com>
2019-01-22 11:27:49 +0100

    adding done

---
+++ week-02/day-2/adding/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    // Add two numbers using pointers
+
+    int a = 20;
+    int b = 17;
+
+    int *p_a = &a;
+    int *p_b = &b;
+
+    std::cout << *p_a + *p_b;
+
+    return 0;
+}
