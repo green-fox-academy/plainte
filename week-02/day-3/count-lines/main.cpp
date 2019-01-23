be02de8fd1555c9d5cb490308e0d02e4a1200b7e
Daniel Molnar <molnard0412@gmail.com>
2019-01-23 15:30:12 +0100

    count-lines fixed, write-multiple-lines started

--- week-02/day-3/count-lines/main.cpp
+++ week-02/day-3/count-lines/main.cpp
@@ -2,28 +2,26 @@
 #include <fstream>
 #include <string>
 
+int readFile(std::string& pass) {
+    std::ifstream file(pass);
+    if (file.is_open()) {
+        std::string data;
+        int lines = 0;
+        while (getline(file, data)) {
+            lines++;
+        }
+        file.close();
+        return lines;
+    } else {
+        return 0;
+    }
+}
 
-int main () {
+int main() {
     // Write a function that takes a filename as string,
     // then returns the number of lines the file contains.
     // It should return zero if it can't open the file
-    try {
-        std::ifstream file;
-        file.open("my-file.txt");
-        if(file.is_open()) {
-            std::string data;
-            int lines = 0;
-            while (getline(file, data)) {
-                lines++;
-            }
-            std::cout << lines;
-            file.close();
-        } else {
-            throw 0;
-        }
-    } catch (...) {
-        std::cout << "zero";
-    }
-
+    std::string pass = "my-file.txt";
+    std::cout << readFile(pass);
     return 0;
 }
---
+++ week-02/day-3/write-multiple-lines/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Create a function that takes 3 parameters: a path, a word and a number,
+// than it should write to a file.
+// The path parameter should describes the location of the file.
+// The word parameter should be a string, that will be written to the file as lines
+// The number parameter should describe how many lines the file should have.
+// So if the word is "apple" and the number is 5, than it should write 5 lines
+// to the file and each line should be "apple"
+void writeInFile(std::string& path, )
+int main() {
+    std::string path = "apple.txt";
+    std::string apple = "apple";
+    int lines = 5;
+
+
+    return 0;
+}
