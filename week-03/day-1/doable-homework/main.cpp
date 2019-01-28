f8fb6f9523f580ea771c9d877a16958067fce1c1
Daniel Molnar <molnard0412@gmail.com>
2019-01-28 19:57:55 +0100

    doable homework done

--- week-03/day-1/doable-homework/main.cpp
+++ week-03/day-1/doable-homework/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* args[]) {
+    std::string tree01 = "arborvitae, ash grey, two, female";
+    std::string tree02 = "hawthorn, dark brown, three, male";
+    std::string tree03 = "pin cherry, reddish brown, four, female";
+    std::string tree04 = "butternut, light gray, one, male";
+    std::string tree05 = "american elm, dark gray, five, male";
+
     return 0;
 }
