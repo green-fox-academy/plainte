add9a07d7518647fc43c579bc1870b894f9a7820
Daniel Molnar <molnard0412@gmail.com>
2019-01-29 14:50:06 +0100

    BlogPost done

--- week-03/day-2/blog-post/BlogPost.cpp
+++ week-03/day-2/blog-post/BlogPost.cpp
@@ -1,1 +1,8 @@
 #include "BlogPost.h"
+
+BlogPost::BlogPost(std::string theAuthorName, std::string theTitle, std::string theText, std::string thePubDate) {
+    authorName = theAuthorName;
+    title = theTitle;
+    text = theText;
+    publicationDate = thePubDate;
+}
--- week-03/day-2/blog-post/main.cpp
+++ week-03/day-2/blog-post/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "BlogPost.h"
 
 int main(int argc, char * argv[]) {
     /*- Create a few blog post objects:
@@ -10,7 +11,23 @@ int main(int argc, char * argv[]) {
             - Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention.
               When I asked to take his picture outside one of IBM’s New York City offices, he told me that
               he wasn’t really into the whole organizer profile thing.
-
      */
+
+    BlogPost object1("John Doe",
+            "\"Lorem Ipsum\"",
+            "Lorem ipsum dolor sit amet.",
+            "2000.05.04.");
+    BlogPost object2("Tim Urban",
+            "\"Wait but why\"",
+            "A popular long-form, stick-figure-illustrated blog about almost everything.", 
+            "2010.10.10.");
+    BlogPost object3("William Turton",
+            "\"One Engineer Is Trying to Get IBM to Reckon With Trump\"",
+            "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. "
+            "When I asked to take his picture outside one of IBM’s New York City offices, "
+            "he told me thathe wasn’t really into the whole organizer profile thing.",
+            "2017.03.28.");
+
+    std::cout << object3.title << std::endl;
     return 0;
 }
