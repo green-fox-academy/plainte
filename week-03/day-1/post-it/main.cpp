6cd82ae7df86c0a2afb77c64cf0f489d95f46ed7
Daniel Molnar <molnard0412@gmail.com>
2019-01-29 14:06:23 +0100

    post it done

---
+++ week-03/day-1/post-it/main.cpp
@@ -0,0 +1,10 @@
+#include "cmake-build-debug/PostIt.h"
+#include <iostream>
+
+int main(int argc, char * args[]) {
+    PostIt PostIt1("orange", "Idea 1", "blue");
+    PostIt PostIt2("pink", "Awesome", "black");
+    PostIt PostIt3("yellow", "Superb!", "green");
+
+    return 0;
+}
