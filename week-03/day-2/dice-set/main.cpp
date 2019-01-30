66b4d85f7fdde15b3acfd51be402aa35aa7f9815
Daniel Molnar <molnard0412@gmail.com>
2019-01-30 11:00:17 +0100

    domino done

--- week-03/day-2/dice-set/main.cpp
+++ week-03/day-2/dice-set/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* args[])
     // You have a `DiceSet` class which has a list for 6 dices
     // You can roll all of them with roll()
     // Check the current rolled numbers with getCurrent()
-    // You can reroll with roll()
+    // You can re-roll with roll()
     // Your task is to roll the dices until all of the dices are 6
     DiceSet diceSet;
     diceSet.roll();
