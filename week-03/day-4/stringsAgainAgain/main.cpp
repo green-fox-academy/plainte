3fbff43dc15e7f96962be0535cb93dbd07abc687
Daniel Molnar <molnard0412@gmail.com>
2019-01-31 16:04:14 +0100

    stringsagain and again done

--- week-03/day-4/stringsAgainAgain/main.cpp
+++ week-03/day-4/stringsAgainAgain/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 
-// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+// Given a string, compute recursively a new string where all the
+// adjacent chars are now separated by a '*'.
 
-std::string changeRecursively(std::string x, unsigned int length)
-{   char k = '*';
+std::string insertRecursively(std::string x, unsigned int length)
+{
     if (length == 0) {
         return x;
-    } else if (x[length - 1] == 'x'){
-        x.erase(length - 1, 1);
-        return changeRecursively(x, length - 1);
     } else {
-        return changeRecursively(x, length - 1);
+        x.insert(length - 1, 1, '*');
+        return insertRecursively(x, length - 1);
     }
 
 }
 int main() {
     std::string stringx = "xer xor or xer and xl osssx";
     unsigned int size = stringx.length();
-    std::cout << changeRecursively(stringx, size);
+    std::cout << insertRecursively(stringx, size);
 
     return 0;
 }
