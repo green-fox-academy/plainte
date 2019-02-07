4cc36503d885ba99d9d500ba797292a258a422c0
Daniel Molnar <molnard0412@gmail.com>
2019-02-07 18:56:11 +0100

    function changed for better understanding / cleanliness, also uppercase letters now transform to lower so testing should be better

--- week-04/day-4/count-letters/countLetters_lib/countLetters_lib.cpp
+++ week-04/day-4/count-letters/countLetters_lib/countLetters_lib.cpp
@@ -1,12 +1,17 @@
+#include <algorithm>
 #include "countLetters_lib.h"
 
 std::map<char, int> countLetters(std::string text)
 {
-    std::map<char, int> charactersOccur;
+    std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+    std::map<char, int> mapC;
     for (unsigned int i = 0; i < text.size(); i++){
-        if (charactersOccur.find(text.at(i)) != charactersOccur.end() ){
-            charactersOccur[text.at(i)] += 1;
-        } else charactersOccur.insert(std::make_pair(text.at(i), 1));
+        char c = text.at(i);
+        if (mapC.find(c) != mapC.end() ){
+            mapC[c] += 1;
+        } else {
+            mapC.insert(std::make_pair(c, 1));
+        }
     }
-    return charactersOccur;
+    return mapC;
 }
--- week-04/day-4/count-letters/countLetters_tests/countLetters_tests.cpp
+++ week-04/day-4/count-letters/countLetters_tests/countLetters_tests.cpp
@@ -5,19 +5,19 @@
 
 TEST(TestCountMethod, LettersOccursMatchLetters)
 {
-    std::map <char, int> testMapOfLetters = countLetters("almafa");
-    ASSERT_EQ(testMapOfLetters['a'], 3);
+    std::map <char, int> testMap = countLetters("almafa");
+    ASSERT_EQ(testMap['a'], 3);
 }
 
 TEST(TestCountMethod2, LettersOccursMatchLetters2)
 {
-    std::map <char, int> testMapOfLetters;
-    testMapOfLetters.insert(std::make_pair('s', 2));
-    testMapOfLetters.insert(std::make_pair('y', 1));
-    testMapOfLetters.insert(std::make_pair('l', 2));
-    testMapOfLetters.insert(std::make_pair('a', 1));
-    testMapOfLetters.insert(std::make_pair('b', 1));
-    testMapOfLetters.insert(std::make_pair('u', 1));
+    std::map <char, int> testMap;
+    testMap.insert(std::make_pair('s', 3));
+    testMap.insert(std::make_pair('y', 1));
+    testMap.insert(std::make_pair('l', 2));
+    testMap.insert(std::make_pair('a', 1));
+    testMap.insert(std::make_pair('b', 1));
+    testMap.insert(std::make_pair('u', 1));
 
-    ASSERT_EQ(countLetters("syllabus"), testMapOfLetters);
+    ASSERT_EQ(countLetters("syllabusS"), testMap);
 }
