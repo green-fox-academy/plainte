4a07d295df7da307a0f6b7536c821d6707ccc022
Daniel Molnar <molnard0412@gmail.com>
2019-02-07 16:32:56 +0100

    sum added to apples

--- week-04/day-4/Apples/Apples_lib/myApplesClass.cpp
+++ week-04/day-4/Apples/Apples_lib/myApplesClass.cpp
@@ -4,3 +4,12 @@ std::string getApple()
 {
     return "apple";
 }
+
+int sum(std::vector<int> elements)
+{
+    int total = 0;
+    for (unsigned int i = 0; i < elements.size(); i++) {
+        total += elements[i];
+    }
+    return total;
+}
--- week-04/day-4/Apples/Apples_tests/myApplesTests.cpp
+++ week-04/day-4/Apples/Apples_tests/myApplesTests.cpp
@@ -1,7 +1,37 @@
 #include <gtest/gtest.h>
 #include "myApplesClass.h"
 
-TEST(apple_check, test_apple)
+TEST(appleMethod, test_string) //test getApple()
 {
     ASSERT_STREQ(getApple().c_str(), "apple");
+}
+
+TEST(sumMethod, test_vector) //list of integers
+{
+    std::vector<int> testElements = {1, 2, 3, 4, 5};
+    ASSERT_EQ(sum(testElements), 15);
+}
+
+TEST(sumMethod2, test_vector2) //empty list
+{
+    std::vector<int> testElements;
+    ASSERT_EQ(sum(testElements), 0);
+}
+
+TEST(sumMethod3, test_vector3) //list with 1 element
+{
+    std::vector<int> testElements = {5};
+    ASSERT_EQ(sum(testElements), 5);
+}
+
+TEST(sumMethod4, test_vector4) //list with multiple elements
+{
+    std::vector<int> testElements = {5, 2, 10, 4};
+    ASSERT_EQ(sum(testElements), 21);
+}
+
+TEST(sumMethod5, test_vector5) //negative sum
+{
+    std::vector<int> testElements = {-20, 4, 7, -3};
+    ASSERT_EQ(sum(testElements), -12);
 }
