5e16dad68d53f8be29a784e283439e0b06fbc5d8
Daniel Molnar <molnard0412@gmail.com>
2019-02-25 12:44:52 +0100

    old-enough done

---
+++ week-06/day-1/old-enough/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int ifOldEnough(int age);
+
+int main()
+{
+    // Create a program which asks for the age of the user and stores it
+    // Create a function which takes that age as a parameter and returns if the user is
+    // old enough to buy himself alcohol in Hungary
+    int age;
+    printf("Please enter your age!\n");
+    scanf("%d", &age);
+
+    printf("%d", ifOldEnough(age));
+    return 0;
+}
+
+int ifOldEnough(int age)
+{
+    if (age > 17) {
+        printf("Congrats, you can buy alcohol in Hungary!\n");
+        return age;
+    } else {
+        printf("Sorry, you aren't old enough to buy alcohol in Hungary!\n");
+        return -1;
+    }
+}
