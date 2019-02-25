2f3be228dcf6b76628e78f118740dbe69ff256c1
Daniel Molnar <molnard0412@gmail.com>
2019-02-25 13:31:21 +0100

    are-the-same in progress

---
+++ week-06/day-1/are-the-same/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compareStrings(char * str1, char * str2);
+
+int main()
+{
+    // Create a program which asks for two strings and stores them
+    // Create a function which takes two strings as parameters and
+    // returns 1 if the two strings are the same and 0 otherwise
+    // Try to erase small and uppercase sensitivity.
+    char str1[100];
+    char str2[100];
+    printf("Please enter two strings!\n");
+    scanf("%s %s", str1, str2);
+
+    if (compareStrings(str1, str2) == 1) {
+        printf("The two strings you've entered are equal!\n");
+    } else {
+        printf("The two strings you've entered are not equal!\n");
+    }
+    return 0;
+}
+
+int compareStrings(char *str1, char *str2)
+{
+    if (strcmp(str1, str2) == 0) {
+        return 1;
+    }
+    return 0;
+}
