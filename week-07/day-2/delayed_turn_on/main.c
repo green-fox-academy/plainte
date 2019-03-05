46e47e21bc888e40b00db484b85e1186127d261d
Daniel Molnar <molnard0412@gmail.com>
2019-03-05 14:58:03 +0100

    delayed-turn-on done

---
+++ week-07/day-2/delayed_turn_on/main.c
@@ -0,0 +1,33 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+			
+
+int main(void)
+{
+	HAL_Init();
+
+	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+
+	BSP_LED_Init(LED1);
+	int count = 0;
+	while (1) {
+		if (BSP_PB_GetState(BUTTON_KEY) && count < 5) {
+			count++;
+			HAL_Delay(150);
+		} else if (count >= 5) {
+			BSP_LED_On(LED1);
+		}
+	}
+}
+
